595fd072547d9c24b4a48b058e675740a42dab7c
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-03-25 12:38:21 +0200

    add img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import tooth from "./images/dental-2.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <h3>Hallo dent</h3>
+      <img src={tooth} style={{ width: "20px" }} />
     </div>
   );
 }
