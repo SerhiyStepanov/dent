33b9a7591f414397ba6c2ee8c8313e2efc94eba8
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-03-26 22:08:42 +0200

    add contacts

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,4 +1,10 @@
-import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaPhoneAlt,
+} from "react-icons/fa";
 import s from "./contacts.module.css";
 
 export default function Contacts() {
@@ -6,19 +12,59 @@ export default function Contacts() {
     <div className={s.thumb}>
       <b>Наша адреса:</b>
       <address>
-        <p className={s.text}>м.Львів, вул.Манастирського 2 / 1 </p>
+        <div className={s.box}>
+          <FaMapMarkerAlt
+            style={{
+              display: "flex",
+              marginRight: "10px",
+              fontSize: "16px",
+              fill: "var(--accent-color)",
+            }}
+          />
+          <p className={s.text}>м.Львів, вул.Манастирського 2 / 1 </p>
+        </div>
 
-        <a href="mailto:adnanrasho@gmail.com" className={s.link}>
-          adnanrasho@gmail.com
-        </a>
-        <br />
-        <a href="tel:+380676700770" className={s.link}>
-          +38 067 670 0770
-        </a>
-        <br />
-        <a href="tel:+380634745565" className={s.link}>
-          +38 063 474 5565
-        </a>
+        <div className={s.box}>
+          <FaEnvelope
+            style={{
+              display: "flex",
+              marginRight: "10px",
+              fontSize: "16px",
+              fill: "var(--accent-color)",
+            }}
+          />
+          <a href="mailto:adnanrasho@gmail.com" className={s.link}>
+            adnanrasho@gmail.com
+          </a>
+        </div>
+
+        <div className={s.box}>
+          <FaPhoneAlt
+            style={{
+              display: "flex",
+              marginRight: "10px",
+              fontSize: "16px",
+              fill: "var(--accent-color)",
+            }}
+          />
+          <a href="tel:+380676700770" className={s.link}>
+            +38 067 670 0770
+          </a>
+        </div>
+
+        <div className={s.box}>
+          <FaPhoneAlt
+            style={{
+              display: "flex",
+              marginRight: "10px",
+              fontSize: "16px",
+              fill: "var(--accent-color)",
+            }}
+          />
+          <a href="tel:+380634745565" className={s.link}>
+            +38 063 474 5565
+          </a>
+        </div>
       </address>
       <div className={s.iconBox}>
         <a href="https://www.facebook.com/AdnanDentLviv" className={s.iconLink}>
@@ -34,3 +80,8 @@ export default function Contacts() {
     </div>
   );
 }
+// FaEnvelope
+
+// FaMapMarkerAlt
+
+// FaPhoneAlt
