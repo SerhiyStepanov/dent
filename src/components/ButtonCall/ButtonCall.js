dc4ea58d44f3693eb84f59fc991a2f641031a611
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-03-29 15:22:06 +0300

    add href button call

--- src/components/ButtonCall/ButtonCall.js
+++ src/components/ButtonCall/ButtonCall.js
@@ -4,7 +4,7 @@ import s from "./buttonCall.module.css";
 export default function ButtonCall() {
   return (
     <div className={s.thumb}>
-      <a href="" className={s.link}>
+      <a href="tel:+380676700770" className={s.link}>
         <FaPhoneAlt className={s.icon} />
       </a>
     </div>
