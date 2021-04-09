b9b41f19db47abecda75283f8a555ade100ca6ab
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-04-09 13:23:58 +0300

    add team json and card team

---
+++ src/components/Team/Team.js
@@ -0,0 +1,20 @@
+import team from "../../team.json";
+import s from "./team.module.css";
+
+export default function Team({ children }) {
+  return (
+    <div>
+      <ul className={s.wrapper}>
+        {team.map(({ name, title, descr }, index) => (
+          <li key={index} className={s.list}>
+            <div className={s.thumb}>
+              <p className={s.name}>{name}</p>
+              <p className={s.title}>{title}</p>
+              <p className={s.descr}>{descr}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- src/views/TeamViews.js
+++ src/views/TeamViews.js
@@ -1,12 +1,14 @@
 import BoxView from "../components/BoxView";
+import Team from "../components/Team";
 
-export default function TeamViews() {
+export default function TeamViews({ children }) {
   return (
     <div>
       <BoxView>
         <h3 style={{ textAlign: "center", color: "var(--dark-color)" }}>
           Наші лікарі
         </h3>
+        <Team />
       </BoxView>
     </div>
   );
