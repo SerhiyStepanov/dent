d1a0f21257f97814994d28a387331c41e10a72b4
SerhiyStepanov <63470231+SerhiyStepanov@users.noreply.github.com>
2021-05-01 19:12:29 +0300

    add photo

--- src/views/HomeViews.js
+++ src/views/HomeViews.js
@@ -1,6 +1,8 @@
 import BoxView from "../components/BoxView";
 import LinkGoTo from "../components/LinkGoTo";
 import CardTeam from "../components/CardTeam";
+import office from "../images/office.jpg";
+import officeVertical from "../images/office-1.jpg";
 
 export default function HomeViews({ children }) {
   return (
@@ -18,16 +20,18 @@ export default function HomeViews({ children }) {
       <p style={{ textAlign: "center" }}>Ми робимо вашу посмішку краще.</p>
 
       <div style={{ marginBottom: "60px" }}>
-        <h4 style={{ textAlign: "center", fontSize: "18px" }}>Спеціалісти:</h4>
-
         <CardTeam />
 
         <LinkGoTo text={"Детальніше"} props={"/team"} />
       </div>
 
       <div>
-        <h4 style={{ textAlign: "center", fontSize: "18px" }}>Послуги:</h4>
-        <p>
+        {window.innerWidth > 767 ? (
+          <img src={office} alt="office" />
+        ) : (
+          <img src={officeVertical} alt="office" />
+        )}
+        <p style={{ textAlign: "center" }}>
           Ми надаємо широкий спектр стоматологічних послуг діагностики та
           лікування професійного рівня.
         </p>
